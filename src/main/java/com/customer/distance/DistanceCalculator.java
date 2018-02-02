ba6cb9e968feabab36031e8eb6a01abe7bc2c9fe
“Vishal <vishal_nagpure@symantec.com>
2018-02-02 13:07:28 +0530

    Correct formula for calculating distance between 2 points

--- src/main/java/com/customer/distance/DistanceCalculator.java
+++ src/main/java/com/customer/distance/DistanceCalculator.java
@@ -1,5 +1,7 @@
 package com.customer.distance;
 
+import com.customer.entity.Location;
+
 public interface DistanceCalculator {
 
     int calculateDistanceInKilometer(Location customerLocation, Location officeLocation);
--- src/main/java/com/customer/distance/DistanceCalculatorImpl.java
+++ src/main/java/com/customer/distance/DistanceCalculatorImpl.java
@@ -1,22 +1,27 @@
 package com.customer.distance;
 
+import com.customer.entity.Location;
+
 public class DistanceCalculatorImpl implements DistanceCalculator {
 
-    public static final int AVERAGE_RADIUS_OF_EARTH_KM = 6371;
+    private static final int AVERAGE_RADIUS_OF_EARTH_KM = 6371;
 
+    /*
+        https://en.wikipedia.org/wiki/Great-circle_distance
+        Delta sigma = arccos(sin(theta1) * sin(theta2) + cos(theta1) * cos(theta2) * cos(Delta gamma)).
+     */
     @Override
     public int calculateDistanceInKilometer(Location customerLocation, Location officeLocation) {
 
-        final double latitudeDistance = Math.toRadians(customerLocation.getLatitude() - officeLocation.getLatitude());
-        final double longitudeDistance = Math.toRadians(customerLocation.getLongitude() - officeLocation.getLongitude());
-        final double sinLatitude = Math.sin(latitudeDistance / 2);
-        final double sinLongitude = Math.sin(longitudeDistance / 2);
+        final double sinLatitudeCustomer = Math.sin(Math.toRadians(customerLocation.getLatitude()));
+        final double sinLatitudeOffice = Math.sin(Math.toRadians(officeLocation.getLatitude()));
+        final double cosinLatitudeCustomer = Math.cos(Math.toRadians(customerLocation.getLatitude()));
+        final double cosinLatitudeOffice = Math.cos(Math.toRadians(officeLocation.getLatitude()));
 
-        double a = sinLatitude * sinLatitude +
-                (Math.cos(Math.toRadians(customerLocation.getLatitude()))
-                        * Math.cos(Math.toRadians(officeLocation.getLatitude()))
-                        * sinLongitude * sinLongitude);
-        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-        return (int) (AVERAGE_RADIUS_OF_EARTH_KM * c);
+        final double cosinlongitudeDelta = Math.cos(Math.abs(Math.toRadians(customerLocation.getLongitude()) - Math.toRadians(officeLocation.getLongitude())));
+
+        final double centralAngel = Math.acos((sinLatitudeCustomer * sinLatitudeOffice) + (cosinLatitudeCustomer * cosinLatitudeOffice * cosinlongitudeDelta));
+
+        return (int) (AVERAGE_RADIUS_OF_EARTH_KM * centralAngel);
     }
 }
